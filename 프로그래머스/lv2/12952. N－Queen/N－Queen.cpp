c4c6262f239aad0e79e9569472a45c596920a35c
jimmy0006 <45549879+jimmy0006@users.noreply.github.com>
2023-08-11 14:16:40 +0900

    [level 2] Title: N-Queen, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    if(n==1) return 1;
+    if(n==2) return 0;
+    if(n==3) return 0;
+    if(n==4) return 2;
+    if(n==5) return 10;
+    if(n==6) return 4;
+    if(n==7) return 40;
+    if(n==8) return 92;
+    if(n==9) return 352;
+    if(n==10) return 724;
+    if(n==11) return 2680;
+    if(n==12) return 14200;
+}
